4192419c20738e49eba47860d37de0db16635dd9
Emmanuel Epie <eepien@gmail.com>
2025-01-13 22:07:05 +0000

    Conflict addressed.

--- shell.sh
+++ shell.sh
@@ -31,12 +31,8 @@ echo " Sum of $num1 and $num2 is $ans" #just adding a comment here. git
 # 
 #These are just changes on geature/updates. 
 #This is another change to feature/update
-#This is a 3rd change to feature/update
-<<<<<<< HEAD
+#This is a 3rd change to feature/updat
 #Updated changes for v2 and V2
-=======
 #Updated changes for v2
 #Adding whatsapp
-
 #Adding feature/upgrade
->>>>>>> develop
